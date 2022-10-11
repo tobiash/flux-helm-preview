ba12a5afba869bc826557eeef051057438ff2fde
Tobias Habermann <thabermann@gmail.com>
2022-10-11 22:01:19 +0200

    allow write output to file

--- pkg/action/action.go
+++ pkg/action/action.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"html/template"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -17,18 +19,27 @@ import (
 )
 
 type Config struct {
-	Helm           bool
-	Kustomizations []string
-	RepoA          string
-	RepoB          string
+	Helm             bool
+	Kustomizations   []string
+	RepoA            string
+	RepoB            string
+	WriteMarkdown    string
+	MarkdownTemplate string
 }
 
 type Action struct {
-	ctx    context.Context
-	cfg    Config
-	log    logr.Logger
-	action *githubactions.Action
+	ctx        context.Context
+	cfg        Config
+	log        logr.Logger
+	action     *githubactions.Action
 	helmRunner *helmrender.Runner
+}
+
+type MarkdownContext struct {
+	Diff           string
+	RepoA          string
+	RepoB          string
+	Kustomizations []string
 }
 
 func NewFromInputs(action *githubactions.Action) (*Config, error) {
@@ -49,6 +60,8 @@ func NewFromInputs(action *githubactions.Action) (*Config, error) {
 			cfg.Kustomizations = append(cfg.Kustomizations, ks)
 		}
 	}
+	cfg.WriteMarkdown = action.GetInput("write-markdown")
+	cfg.MarkdownTemplate = action.GetInput("markdown-template")
 	return cfg, nil
 }
 
@@ -82,7 +95,29 @@ func (a *Action) Run() error {
 	}
 	// a.action.AddStepSummary(fmt.Sprintf("```\n%s\n```", string(buf.Bytes())))
 	a.action.SetOutput("diff", string(buf.Bytes()))
+	if a.cfg.WriteMarkdown != "" {
+		return a.writeMarkdown(string(buf.Bytes()))
+	}
 	return nil
+}
+
+func (a *Action) writeMarkdown(diff string) error {
+	mdCtx := MarkdownContext{
+		Diff:           diff,
+		RepoA:          a.cfg.RepoA,
+		RepoB:          a.cfg.RepoB,
+		Kustomizations: a.cfg.Kustomizations,
+	}
+	tpl, err := template.New("markdown").Parse(a.cfg.MarkdownTemplate)
+	if err != nil {
+		return fmt.Errorf("error parsing markdown template: %w", err)
+	}
+	f, err := os.Create(a.cfg.WriteMarkdown)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return tpl.Execute(f, &mdCtx)
 }
 
 func (a *Action) loadRepo(repo string) (*render.Render, error) {
